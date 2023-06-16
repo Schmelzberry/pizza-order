b84f454c12989ec78c7b1451f90c4363c38c1026
Schmelzberry <jelsberry96@gmail.com>
2023-06-16 10:38:00 -0700

    create orderform object, test passed

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-Describe: itzaPizza ()
+Describe: Pizza ()
 
 Test: It will create a pizza object with two keys, price and toppings.
 
-code: const samplePizza = new isaPizza(["pepperoni", "pineapple"], medium)
+code: let testPizza1 = new Pizza("sausage", "small");
 
-Expected Output: isaPizza { toppings: ["ham", "pineapple"], "medium" }
+Expected Output: testPizza1 = { toppings: "sausage", size: "small" }
+
+Describe: Orderform()
+
+Test: It will create an empty object for future pizzas to go into.
+
+code: let newPizzaForm = new Orderform();
+
+Expected Output: Orderform {pizza: {...} }
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-// Business Logic
+// Business Logic for Pizza Object ----------
 
 function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
 
+
+let test1Pizza = new Pizza("sausage", "small");
+
+
+//Business Logic for Orderform
+function Orderform() {
+    this.pizza = {};
+}
+let newPizzaForm = new Orderform();
+
 Pizza.prototype.pricePizza = function () {
+    const toppingsCost = (this.toppings)
+    const sizePrice = (this.sizePrice)
     return this.toppings + this.size;
-    console.log("success!")
 };
+
+function calculateToppingsCost(toppings) {
+    let 
+}
