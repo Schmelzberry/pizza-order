6855b75021ec788e4691357d63a3df35ed672a41
Schmelzberry <jelsberry96@gmail.com>
2023-06-16 13:12:30 -0700

    IDs are being created, but in wrong object

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 //Business Logic for Orderform
 function Orderform() {
     this.pizzas = {};
+    this.currentId = 0;
 }
 let newPizzaForm = new Orderform();
 let test2Pizza = new Pizza("mushroom", "medium")
 
 // add pizza object to Orderform
 Orderform.prototype.addPizza = function(pizza) {
-    this.pizzas[pizza.size] = pizza;
+    pizza.id = this.assignId();
+    this.pizzas[pizza.id] = pizza;
 };
-
-
+// assign an id to each pizza
+Orderform.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+};
 
 
 // Business Logic for Pizza Object ----------
