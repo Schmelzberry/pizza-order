eea25c7c57a543efb40e6bce7a27d126f1e71b49
Schmelzberry <jelsberry96@gmail.com>
2023-06-16 12:16:59 -0700

    test passed, adding pizzas to order form

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-Describe: Pizza ()
+Describe: function Pizza (toppings, size)
 
 Test: It will create a pizza object with two keys, price and toppings.
 
 code: let testPizza1 = new Pizza("sausage", "small");
 
 Expected Output: testPizza1 = { toppings: "sausage", size: "small" }
+
+Describe: detailsPizza()
+
+Test: It will concatenate details of pizza into one string
+
+code: let test1Pizza = new Pizza("sausage", "small");
+console.log(test1Pizza.detailsPizza());
+
+Expected Result: sausage small
 
 Describe: Orderform()
 
@@ -14,4 +23,5 @@ code: let newPizzaForm = new Orderform();
 
 Expected Output: Orderform {pizza: {...} }
 
-Describe: 
+Describe: detailsPizza
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
+//Business Logic for Orderform
+function Orderform() {
+    this.pizzas = {};
+}
+let newPizzaForm = new Orderform();
+let test2Pizza = new Pizza("mushroom", "medium")
+
+// add pizza object to Orderform
+Orderform.prototype.addPizza = function(pizza) {
+    this.pizzas[pizza.size] = pizza;
+};
+
+
+
+
 // Business Logic for Pizza Object ----------
 
 function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
-
+// combines details of pizza into one string
 Pizza.prototype.detailsPizza = function () {
-    return this.toppings + " " + this.size;
+    return this.toppings + ", " + this.size;
 };
 
 let test1Pizza = new Pizza("sausage", "small");
 console.log(test1Pizza.detailsPizza());
 
 
-//Business Logic for Orderform
-function Orderform() {
-    this.pizza = {};
-}
-let newPizzaForm = new Orderform();
-
-Orderform.prototype.addPizza = function(pizza) {
-    this.pizza[pizza.size] = pizza;
-};
 
